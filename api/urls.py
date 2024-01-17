be8a879c6d80ac69bb75d455bc810c1c32b99eaa
Raahim Irfan <raahimirfan20@gmail.com>
2024-01-17 10:22:22 +0500

    added viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import *
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     #path('', apiOverview, name="api-overview"),
-    path('list/', itemList, name="item-list"),
-    path('search/<str:name>/', itemSearch, name="item-search"),
-    path('add/', addItem, name="item-create"),
-    path('delete-exact-match/<str:name>/', deleteExactMatchItem, name="item-delete-exact-match"),
-    path('delete-partial-match/<str:name>/', deletePartialMatchItem, name="item-delete-partial-match"),
+    path('list/', ItemViewSet.as_view({'get': 'itemList'}), name="item-list"),
+    path('search/<str:name>/', ItemViewSet.as_view({'get': 'itemSearch'}), name="item-search"),
+    path('add/', ItemViewSet.as_view({'post': 'addItem'}), name="item-create"),
+    path('delete-exact-match/<str:name>/', ItemViewSet.as_view({'delete': 'deleteExactMatchItem'}), name="item-delete-exact-match"),
+    path('delete-partial-match/<str:name>/', ItemViewSet.as_view({'delete': 'deletePartialMatchItem'}), name="item-delete-partial-match"),
     
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
--- api/views copy.py
+++ api/views copy.py
@@ -4,17 +4,8 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .serializers import ItemSerializer
 from .models import Item
-from django.http import Http404
-
-import logging
-logger = logging.getLogger(__name__)
-
 
 class ItemViewSet(viewsets.ViewSet):
-
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-
     @action(detail=False, methods=['GET'])
     def apiOverview(self, request):
         api_urls = {
@@ -33,7 +24,7 @@ class ItemViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     @action(detail=True, methods=['GET'])
-    def itemSearch(self, request, name=None):
+    def itemSearch(self, request, pk=None):
         item = self.get_object()
         item.search_count += 1
         item.save()
@@ -46,7 +37,6 @@ class ItemViewSet(viewsets.ViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        print(serializer.errors)
         return Response(serializer.errors, status=400)
 
     @action(detail=True, methods=['DELETE'])
@@ -63,16 +53,10 @@ class ItemViewSet(viewsets.ViewSet):
 
     def get_object(self):
         queryset = Item.objects.all()
-        try:
-            obj = queryset.get(name=self.kwargs['name'])
-            self.check_object_permissions(self.request, obj)
-        except Item.DoesNotExist:
-            raise Http404("No item matches the given query.")
+        obj = queryset.get(name=self.kwargs['name'])
+        self.check_object_permissions(self.request, obj)
         return obj
 
     @action(detail=False, methods=['GET'])
     def index(self, request):
         return render(request, 'api/index.html')
-
-
-logging.basicConfig(level=logging.DEBUG)
